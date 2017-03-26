7abaf23d3e2553adcec81b64e5af7c4548718dc7
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-03-26 17:47:28 +0200

    added catch

--- get_environment.py
+++ get_environment.py
@@ -3,6 +3,7 @@ import subprocess
 
 env = {}
 cwd = os.getcwd();
+env["CATCH_ROOT"] = os.path.join(cwd, "catch", "src")
 env["DATE_ROOT"] = os.path.join(cwd, "date", "src")
 env["RAPIDJSON_ROOT"] = os.path.join(cwd, "rapidjson", "install")
 env["SQLITE3_ROOT"] = os.path.join(cwd, "sqlite3", "install")
