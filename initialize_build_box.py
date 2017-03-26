5cc8e9c1651c5f3ca867b6541c190c4bce4b4976
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-03-26 13:38:09 +0200

    sqlite3 build

---
+++ initialize_build_box.py
@@ -0,0 +1,55 @@
+
+import os
+import platform
+import shutil
+import subprocess
+import versions
+
+def call(args, cwd=os.getcwd()):
+    p = subprocess.Popen(args, cwd=cwd)
+    p.wait()
+
+def initialize_directories(dirs):
+    for d in dirs:
+        shutil.rmtree(d, ignore_errors=True)
+    for d in dirs:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+def cmake_generator_for_toolchain(toolchain):
+    if(toolchain == "Win64-MSVC14"):
+        return "Visual Studio 14 2015 Win64"
+    elif((toolchain == "MacOS-Make") or (toolchain == "Linux-Make")):
+        return "Unix Makefiles"
+    raise RuntimeError("Unsupported toolchain: " + toolchain)
+
+
+def build_sqlite3(toolchain):
+    cwd = os.getcwd()
+    source_dir = os.path.join(cwd, "sqlite3", "src")
+    build_dir = os.path.join(cwd, "sqlite3", "build");
+    install_dir = os.path.join(cwd, "sqlite3", "install")
+    initialize_directories([build_dir, install_dir])
+
+    cmake_generator = cmake_generator_for_toolchain(toolchain)
+    call(["cmake", "-G", cmake_generator, source_dir], cwd=build_dir)
+    call(["cmake", "--build", build_dir, "--config", "Release"])
+
+    os.makedirs(os.path.join(install_dir, "include"))
+    shutil.copy(os.path.join(source_dir, "sqlite3.h"), os.path.join(install_dir, "include"))
+    shutil.copy(os.path.join(source_dir, "sqlite3ext.h"), os.path.join(install_dir, "include"))
+    os.makedirs(os.path.join(install_dir, "lib"))
+    if(os.name == "nt"):
+        shutil.copy(os.path.join(build_dir, "Release", "sqlite3.lib"), os.path.join(install_dir, "lib"))
+    else:
+        shutil.copy(os.path.join(build_dir, "libsqlite3.a"), os.path.join(install_dir, "lib"))
+
+if os.name == "nt":
+    toolchain = "Win64-MSVC14"
+elif os.name == "posix":
+    if platform.system() == "Darwin":
+        toolchain = "MacOS-Make"
+    elif platform.system() == "Linux":
+        toolchain = "Linux-Make"
+
+build_sqlite3(toolchain)
---
+++ versions.py
@@ -0,0 +1,6 @@
+library_versions = {
+    "cpprestsdk" : "2.9.1",
+    "sqlite3" : "3.17.0",
+    "sqlpp11" : "0.48",
+    "sqlpp11sqlpp11-connector-sqlite3" : "0.23",
+}
