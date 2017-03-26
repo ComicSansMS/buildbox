862f3df2a63bd39c047bebca34db12d6eaafd4f0
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-03-26 17:31:29 +0200

    added get_environment script

---
+++ get_environment.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+env = {}
+cwd = os.getcwd();
+env["DATE_ROOT"] = os.path.join(cwd, "date", "src")
+env["RAPIDJSON_ROOT"] = os.path.join(cwd, "rapidjson", "install")
+env["SQLITE3_ROOT"] = os.path.join(cwd, "sqlite3", "install")
+env["SQLPP11_ROOT"] = os.path.join(cwd, "sqlpp11", "install")
+env["SQLPP11_CONNECTOR_SQLITE3_SOURCE_ROOT"] = os.path.join(cwd, "sqlpp11-connector-sqlite3", "src")
+
+print("The build box environment is:")
+
+for v, d in env.items():
+    print(v + "=" + d)
