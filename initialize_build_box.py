71354f748866f5700b5dd6fd4201f13fd3692d31
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-03-26 14:06:27 +0200

    builds for sqlpp11

--- initialize_build_box.py
+++ initialize_build_box.py
@@ -44,6 +44,43 @@ def build_sqlite3(toolchain):
     else:
         shutil.copy(os.path.join(build_dir, "libsqlite3.a"), os.path.join(install_dir, "lib"))
 
+def build_sqlpp11(toolchain):
+    cwd = os.getcwd()
+    source_dir = os.path.join(cwd, "sqlpp11", "src")
+    build_dir = os.path.join(cwd, "sqlpp11", "build");
+    install_dir = os.path.join(cwd, "sqlpp11", "install")
+    initialize_directories([build_dir, install_dir])
+
+    cmake_generator = cmake_generator_for_toolchain(toolchain)
+    call(["cmake", "-G", cmake_generator,
+                   "-DHinnantDate_ROOT_DIR=" + os.path.join(cwd, "date", "src"),
+                   "-DENABLE_TESTS=OFF",
+                   "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir],
+         cwd=build_dir)
+    call(["cmake", "--build", build_dir, "--config", "Release"])
+    call(["cmake", "--build", build_dir, "--config", "Release", "--target", "INSTALL"])
+
+def build_sqlpp11_connector_sqlite3(toolchain):
+    cwd = os.getcwd()
+    source_dir = os.path.join(cwd, "sqlpp11-connector-sqlite3", "src")
+    build_dir = os.path.join(cwd, "sqlpp11-connector-sqlite3", "build");
+    install_dir = os.path.join(cwd, "sqlpp11-connector-sqlite3", "install")
+    initialize_directories([build_dir, install_dir])
+
+    cmake_generator = cmake_generator_for_toolchain(toolchain)
+    call(["cmake", "-G", cmake_generator,
+                   "-DDATE_INCLUDE_DIR=" + os.path.join(cwd, "date", "src"),
+                   "-DSQLPP11_INCLUDE_DIR=" + os.path.join(cwd, "sqlpp11", "install", "include"),
+                   "-DSQLITE3_INCLUDE_DIR=" + os.path.join(cwd, "sqlite3", "install", "include"),
+                   "-DSQLITE3_LIBRARY=" + os.path.join(cwd, "sqlite3", "install", "lib", ("sqlite3.lib" if toolchain == "Win64-MSVC14" else "libsqlite3.a")),
+                   "-DCMAKE_INSTALL_PREFIX=" + install_dir, source_dir],
+         cwd=build_dir)
+    call(["cmake", "--build", build_dir, "--config", "Debug", "--target", "sqlpp11-connector-sqlite3"])
+    call(["cmake", "--build", build_dir, "--config", "Release", "--target", "sqlpp11-connector-sqlite3"])
+    call(["cmake", "--build", build_dir, "--config", "Release", "--target", "INSTALL"])
+
+
+
 if os.name == "nt":
     toolchain = "Win64-MSVC14"
 elif os.name == "posix":
@@ -53,3 +90,5 @@ elif os.name == "posix":
         toolchain = "Linux-Make"
 
 build_sqlite3(toolchain)
+build_sqlpp11(toolchain)
+build_sqlpp11_connector_sqlite3(toolchain)
--- versions.py
+++ versions.py
@@ -1,4 +1,5 @@
 library_versions = {
+    "date" : "2.1.0",
     "cpprestsdk" : "2.9.1",
     "sqlite3" : "3.17.0",
     "sqlpp11" : "0.48",
