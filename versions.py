3ea3993bf642fe923afb5ab8a1afe7dc26b79609
Andreas Weis <github@ghulbus-inc.de>
2017-03-26 16:22:02 +0200

    added submodules for casablanca and rapidjson

--- versions.py
+++ versions.py
@@ -1,6 +1,7 @@
 library_versions = {
     "date" : "2.1.0",
     "cpprestsdk" : "2.9.1",
+    "rapidjson" : "1.1.0",
     "sqlite3" : "3.17.0",
     "sqlpp11" : "0.48",
     "sqlpp11sqlpp11-connector-sqlite3" : "0.23",
