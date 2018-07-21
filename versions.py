08da47db4c29a475a530bb0cc845ed3a56b3c09c
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-07-21 19:18:50 +0200

    updated sqlite3

--- versions.py
+++ versions.py
@@ -2,7 +2,7 @@ library_versions = {
     "date" : "2.1.0",
     "cpprestsdk" : "2.9.1",
     "rapidjson" : "1.1.0",
-    "sqlite3" : "3.17.0",
+    "sqlite3" : "3.24.0",
     "sqlpp11" : "0.57",
     "sqlpp11sqlpp11-connector-sqlite3" : "0.29",
 }
