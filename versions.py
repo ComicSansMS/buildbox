bf9b1144842954ea7345b9d34e8d578a44407cbc
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-07-21 12:10:26 +0200

    version update sqlpp11 and sqlite connector

--- versions.py
+++ versions.py
@@ -3,6 +3,6 @@ library_versions = {
     "cpprestsdk" : "2.9.1",
     "rapidjson" : "1.1.0",
     "sqlite3" : "3.17.0",
-    "sqlpp11" : "0.48",
-    "sqlpp11sqlpp11-connector-sqlite3" : "0.23",
+    "sqlpp11" : "0.57",
+    "sqlpp11sqlpp11-connector-sqlite3" : "0.29",
 }
